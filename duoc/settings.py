b79ef825f41b491c06c36af2b473b67a45de4e66
Alexlopez98 <alexa.lopezs@duocuc.cl>
2025-09-22 23:51:35 -0300

    Team Test
    últimos retoque antes de envió de evaluación 2

--- duoc/settings.py
+++ duoc/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'duoc.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.oracle',
-        'NAME':'127.0.0.1:1521/xe',
+        'NAME':'127.0.0.1:1521/orcl',
         'USER': 'c###bocarella',
         'PASSWORD': 'bocarella',
         'TEST':{
